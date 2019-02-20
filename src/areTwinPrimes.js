f868ca67baa41561fe5b625c72fda4f4584cc7ef
Jain <rjain15@intul1687206f4.corp.intuit.net>
2019-02-20 11:31:43 +0530

    Solved binToHex. added problem areTwinPrimes

---
+++ src/areTwinPrimes.js
@@ -0,0 +1,13 @@
+/*
+Input: 3 5
+Output: true
+
+Input: 5 11
+Output: false
+
+Input: 9 11
+Output: false
+*/
+export const areTwinPrimes = (int,int) => {
+    
+};
---
+++ src/binToHex.js
@@ -0,0 +1,7 @@
+/*
+Input: 1011
+Output: b
+*/
+export const binToHex = (string) => {
+    return parseInt(string,2).toString(16);
+};
