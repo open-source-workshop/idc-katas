05b34072fbfffa39b8cde94a165c30c0c48ce5fb
sarcastic-sloth <13pramo@gmail.com>
2019-06-02 08:35:01 +0530

    Solved celsiusToFahrenheit and added fahrenheitToCelsius (#18)
    * Create celsiusToFahrenheit.js
    
    * Create fahrenheitToCelsius.js
    
    * Create primeOrComposite.js
    
    * Create nextPerfectSquare.js

---
+++ src/fahrenheitToCelsius.js
@@ -0,0 +1,5 @@
+
+export const fahrenheitToCelsius = (tempFah) => {
+    // TO IMPLEMENT IN ANOTHER PR
+
+}
---
+++ src/nextPerfectSquare.js
@@ -0,0 +1,3 @@
+export const nextPerfectSquare = (num) => {
+    return (Math.pow((Math.floor(Math.sqrt(num)) + 1), 2));
+}
