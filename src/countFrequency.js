195f80b46d22438afe18d324a5477ce45825de43
shashank <47443963+guptashak90@users.noreply.github.com>
2019-06-18 10:52:43 +0530

    Create countFrequency.js

---
+++ src/countFrequency.js
@@ -0,0 +1,13 @@
+export const countFrequency = (string) => {
+    var freq = {};
+    for (var i=0; i<string.length;i++) {
+        var character = string.charAt(i);
+        if (freq[character]) {
+           freq[character]++;
+        } else {
+           freq[character] = 1;
+        }
+    }
+
+    return freq;
+};
